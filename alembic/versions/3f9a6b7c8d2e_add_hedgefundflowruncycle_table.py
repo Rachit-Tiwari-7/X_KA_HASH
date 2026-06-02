fee5337ab5c999b9f5c9424ae541ef68c135d86b
Bit_Blitz <rachit.tiwari.07.off@gmail.com>
2026-06-02 18:53:49 +0530

    Add alembic/versions/3f9a6b7c8d2e_add_hedgefundflowruncycle_table.py

---
+++ alembic/versions/3f9a6b7c8d2e_add_hedgefundflowruncycle_table.py
@@ -0,0 +1,23 @@
+"""Add HedgeFundFlowRunCycle table and update HedgeFundFlowRun
+
+Revision ID: 3f9a6b7c8d2e
+Revises: 2f8c5d9e4b1a
+Create Date: 2024-11-27 10:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '3f9a6b7c8d2e'
+down_revision = '2f8c5d9e4b1a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
